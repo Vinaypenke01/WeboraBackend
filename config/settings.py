58cc09819e900c2e1903fa6e317cc64eba145d0d
Vinaypenke01 <itzvinay8@gmail.com>
2025-12-21 20:41:27 +0530

    added host

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,11 @@ SECRET_KEY = 'django-insecure-change-me-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'weborabackend.onrender.com',
+    'www.weborabackend.onrender.com',
+]
+
 
 
 # Application definition
